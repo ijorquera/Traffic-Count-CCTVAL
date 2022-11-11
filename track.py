631f64ad962250a080ab21ea5360a0ddaabdb7e9
Jesus Ortiz <jesus.ortiz@sansano.usm.cl>
2022-11-11 09:29:46 -0500

    Cambio de logo

--- track.py
+++ track.py
@@ -227,6 +227,15 @@ def detect(opt):
             # Stream results
             im0 = annotator.result()
             if show_vid:
+                logo = cv2.imread('logo.png')
+                size1 = 100
+                size2 =150
+                logo = cv2.resize(logo, (size2, size1))
+                img2gray = cv2.cvtColor(logo, cv2.COLOR_BGR2GRAY)
+                ret, mask = cv2.threshold(img2gray, 1, 255, cv2.THRESH_BINARY)
+                roi = im0[-size1-10:-10, -size2-10:-10]
+                roi[np.where(mask)] = 0
+                roi += logo
                 color = (0, 255, 0)
                 start_point = (0, h - 350)
                 end_point = (int(w/2) - 50, h - 350)
