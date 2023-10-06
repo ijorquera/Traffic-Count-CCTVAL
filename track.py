c2c812a95d5f3360c3bccdf5733162e684d4036b
Ignacio Jorquera <43505842+ijorquera@users.noreply.github.com>
2023-10-06 11:19:51 -0300

    feat: :art: New output txt
    Added a new txt for the output trying to make it a little more interpretable (details on CHANGES.md)

--- track.py
+++ track.py
@@ -210,6 +210,12 @@ def detect(opt):
                                 f.write(('%g ' * 10 + '\n') % (frame_idx + 1, id, bbox_left,  # MOT format
                                                                bbox_top, bbox_w, bbox_h, -1, -1, -1, i))
 
+                            c = int(cls)  # integer class
+                            label = f'{id} {names[c]} {conf:.2f}'                            
+                            # Write results in an interpretable file
+                            with open(txt_path + '_interpretable.csv', 'a') as f:
+                                f.write(f"{i},{frame_idx + 1},{label},{id},{bbox_left},{bbox_top},{bbox_w},{bbox_h}\n")
+                                
                         if save_vid or save_crop or show_vid:  # Add bbox to image
                             c = int(cls)  # integer class
                             label = f'{id} {names[c]} {conf:.2f}'
